452ead5fdb9b613f8eab6daac0d80ca44aca3fa8
mohan <mohan@gmail.com>
2023-02-14 16:47:01 +0530

    profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ExperianceComponent } from './experiance/experiance.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { QualityComponent } from './quality/quality.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"",component:HomeComponent},
+  { path:"home",component:HomeComponent},
+  { path:"profile",component:ProfileComponent},
+  { path:"experiance",component:ExperianceComponent},
+  { path:"contact",component:ContactComponent},
+  { path:"skill",component:ServicesComponent},
+  { path:"education",component:QualityComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { QualityComponent } from './quality/quality.component';
+import { ExperianceComponent } from './experiance/experiance.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+// import { ProtfolioComponent } from './protfolio/protfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HomeComponent,
+    QualityComponent,
+    ExperianceComponent,
+    ServicesComponent,
+    ContactComponent,
+    // ProtfolioComponent
   ],
   imports: [
     BrowserModule,
